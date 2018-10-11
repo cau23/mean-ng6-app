3c017d825e172e2c3984277e78e8f26213552e4c
Chad <chad.a.upchurch@gmail.com>
2018-10-10 23:32:47 -0500

    backend-setup

---
+++ src/app/adunit.service.ts
@@ -0,0 +1,11 @@
+// Ad Unit service to send HTTP requests
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdunitService {
+
+  constructor() { }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CreateComponent } from './components/create/create.component';
 import { IndexComponent } from './components/index/index.component';
 import { EditComponent } from './components/edit/edit.component';
+
+import { AdunitService } './adunit.service';
 
 const routes: Routes = [
   {
@@ -33,9 +36,10 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
-    SlimLoadingBarModule
+    SlimLoadingBarModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ AdunitService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
